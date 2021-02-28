8cd227cb1f8e8c0691cb97efdf9690b41ec85083
enyung <45346786+eyl056@users.noreply.github.com>
2021-03-01 02:43:10 +0900

    API명세서 추가

--- netflixAPI/src/routes/userRoutes.js
+++ netflixAPI/src/routes/userRoutes.js
@@ -1,8 +1,8 @@
 module.exports = function(app) {
     const user = require('../controllers/userController');
     
-    app.route('/app/signUp').post(user.signUp);
-    app.route('/app/signIn').post(user.signIn);
+    app.route('/app/signUp').post(user.signUp); // 회원가입
+    app.route('/app/signIn').post(user.signIn); // 로그인
 
     //app.get('/app/signUp', user.signUp);
 }
